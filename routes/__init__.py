21b91aac499fca6387f270e0d8345e8a40fd2d74
noaman <noaman.kazi@gmail.com>
2018-11-18 18:01:22 +0000

    new changes made

--- routes/__init__.py
+++ routes/__init__.py
@@ -8,3 +8,9 @@ from .tw_analyzer import *
 
 if __name__ == '__main__':                
     app.run(debug=True)
+
+
+
+#AWS code deploy access
+#access key :AKIAITDOVL4MLVPWNHWA
+#Secret key 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
--- routes/yt_downloader.py
+++ routes/yt_downloader.py
@@ -1,9 +1,15 @@
 from flask import render_template
 from . import routes
+from flask_wtf import Form
+from wtforms import TextField
+
+class YouTubeDownloaderForm(Form):
+   name = TextField("URL")
+
 
 @routes.route('/yt',methods=['POST', 'GET'])
 def yt_index():
-	
-	return render_template('yt_downloader.html')
+	form = YouTubeDownloaderForm()	
+	return render_template('yt_downloader.html',form=form)
 
 
